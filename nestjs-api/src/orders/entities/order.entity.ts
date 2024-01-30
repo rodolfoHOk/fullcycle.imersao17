1ef24c6b37ef0efd6aaf0b2b319fee510ff9ba96
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-30 13:31:22 -0300

    feat: nestapi create order and order item entities

--- nestjs-api/src/orders/entities/order.entity.ts
+++ nestjs-api/src/orders/entities/order.entity.ts
@@ -1,1 +1,30 @@
-export class Order {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum OrderStatus {
+  PENDING = 'pending',
+  PAID = 'paid',
+  FAILED = 'failed',
+}
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total: number;
+
+  @Column()
+  client_id: number;
+
+  @Column()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
