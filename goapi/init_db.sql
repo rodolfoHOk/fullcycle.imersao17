4ce2f5bb41e7f0abe1c903212140d8b42bd40f8f
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-29 23:28:20 -0300

    chore: create init db sql

---
+++ goapi/init_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE categories (
+  id varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE products (
+  id varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  price decimal(10,2) NOT NULL,
+  category_id varchar(36) NOT NULL,
+  image_url varchar(255) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_products_categories_idx FOREIGN KEY (category_id) REFERENCES categories(id)
+);
