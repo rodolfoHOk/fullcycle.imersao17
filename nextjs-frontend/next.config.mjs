7899a15ae6df59208694bf7c09bcb14b1472bb69
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-31 13:08:50 -0300

    feat: nextfront create products page

--- nextjs-frontend/next.config.mjs
+++ nextjs-frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        pathname: '/random',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
